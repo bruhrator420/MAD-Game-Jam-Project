94d24f42c4f13f29066e99246848400bd866d9a8
bruhrator420 <n3cr0jr@gmail.com>
2022-02-26 23:31:32 +0000

    SIUUUU

--- Project/Scripts/Level.gd
+++ Project/Scripts/Level.gd
@@ -19,5 +19,5 @@ func _unhandled_input(event):
 
 func _on_Area2D_body_entered(body):
 	print("lole")
-	if body.is_in_group("player"):
+	if body.is_in_group("players"):
 		get_tree().change_scene("res://Scenes/2.tscn")
